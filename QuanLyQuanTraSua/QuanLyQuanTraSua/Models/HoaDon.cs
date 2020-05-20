0d774f5275e3dc9d6b2f6db4cf294e8ec79d83ea
Kiet Tran <44864496+radiun42@users.noreply.github.com>
2020-05-20 10:46:47 +0700

    Thêm vài test case

---
+++ QuanLyQuanTraSua/QuanLyQuanTraSua/Models/HoaDon.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyQuanTraSua.Models
+{
+    public class HoaDon
+    {
+        private int maHoaDon;
+        private string tenHoaDon;
+        private decimal tongTien;
+        private string tinhTrang;
+        private int phieuGiamGia;
+
+        public int MaHoaDon { get => maHoaDon; set => maHoaDon = value; }
+        public string TenHoaDon { get => tenHoaDon; set => tenHoaDon = value; }
+        public decimal TongTien { get => tongTien; set => tongTien = value; }
+        public string TinhTrang { get => tinhTrang; set => tinhTrang = value; }
+        public int PhieuGiamGia { get => phieuGiamGia; set => phieuGiamGia = value; }
+
+        public void KiemTraHopLe()
+        {
+            if (tongTien < 0)
+                throw new Exception("Tong tien bi am");
+            if (!tinhTrang.Equals("Dang Thanh Toan") || !tinhTrang.Equals("Da Thanh Toan") || !tinhTrang.Equals("Huy Thanh Toan"))
+                throw new Exception("Tinh trang khong hop le");
+        }
+
+        public decimal SoTienChenhLechTest()
+        {
+            if (phieuGiamGia <= 0 || phieuGiamGia > 100)
+                throw new ArgumentOutOfRangeException("Phieu giam gia nho hon bang 0 hoac lon hon 100");
+            return TongTien * (100 - phieuGiamGia) / 100;
+        }
+
+        public decimal SoTienChenhLech()
+        {
+            if (phieuGiamGia <= 0 || phieuGiamGia > 100)
+                throw new ArgumentOutOfRangeException("Phieu giam gia nho hon bang 0 hoac lon hon 100");
+
+            decimal tienDaGiam = this.SoTienChenhLechTest();
+
+            return TongTien + tienDaGiam;
+        }
+    }
+}
---
+++ QuanLyQuanTraSua/QuanLyQuanTraSuaTests/Models/HoaDonTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuanLyQuanTraSua.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyQuanTraSua.Models.Tests
+{
+    [TestClass()]
+    public class HoaDonTests
+    {
+        [TestMethod()]
+        public void KiemTraHopLeTestException()
+        {
+            HoaDon hoaDon = new HoaDon()
+            {
+                MaHoaDon = 1,
+                TenHoaDon = "Hoa don thanh toan",
+                TongTien = 52000,
+                PhieuGiamGia = 5,
+                TinhTrang = "thanh toan"
+            };
+
+            try
+            {
+                hoaDon.KiemTraHopLe();
+            }
+            catch (Exception e)
+            {
+                StringAssert.Contains(e.Message, "Tinh trang khong hop le");
+                return;
+            }
+
+            Assert.Fail("No exception !");
+        }
+
+        [TestMethod()]
+        public void TongTienGiamGiaTest()
+        {
+            HoaDon hoaDon = new HoaDon()
+            {
+                MaHoaDon = 1,
+                TenHoaDon = "Hoa don thanh toan",
+                TongTien = 100000,
+                PhieuGiamGia = 10,
+                TinhTrang = "Dang Thanh Toan"
+            };
+
+            Assert.AreEqual(90000, hoaDon.SoTienChenhLechTest());
+        }
+
+        [TestMethod()]
+        public void SoTienChenhLechTest()
+        {
+            HoaDon hoaDon = new HoaDon()
+            {
+                MaHoaDon = 1,
+                TenHoaDon = "Hoa don thanh toan",
+                TongTien = 100000,
+                PhieuGiamGia = 10,
+                TinhTrang = "Dang Thanh Toan"
+            };
+
+            Assert.AreEqual(1000, hoaDon.SoTienChenhLech());
+        }
+    }
+}
